aa5f65ed35e274b3ccdfcdbc606d7fcbbd6df686
Daniel Schneider <schneider@volkskammer.org>
2024-02-07 15:12:16 +0100

    started

--- src/app.py
+++ src/app.py
@@ -10,6 +10,7 @@ import chainlit as cl
 import base64
 import promptflow as pf
 
+
 @cl.on_chat_start
 def start_chat():
     print("starting chat")
@@ -19,10 +20,17 @@ def start_chat():
         [],
     )
 
-    cl.user_session.set(
-        "prompt_flow",
+    promptflows = [
         os.path.join(os.path.dirname(__file__), 'functions_flow'),
+        os.path.join(os.path.dirname(__file__), 'autogen_flow')
+    ]
+
+    config = dict(
+        active_promptflow = promptflows[0],
+        promptflows = promptflows,
     )
+    cl.user_session.set("config", config)
+    
 
 def show_images(image):
     # image is a dict of length 1
@@ -33,7 +41,7 @@ def show_images(image):
     if image_type.endswith(";base64"):
         decoded_image = base64.b64decode(image_content)
     elif image_type.endswith(";path"):
-        prompt_flow = cl.user_session.get("prompt_flow")
+        prompt_flow = cl.user_session.get("config")["active_promptflow"]
         decoded_image = open(os.path.join(prompt_flow, image_content), "rb").read()
     else:
         raise Exception("Unknown image type: " + image_type)
@@ -49,7 +57,7 @@ def show_images(image):
 
 @cl.step(type="llm")
 async def call_promptflow(chat_history, message):
-    prompt_flow = cl.user_session.get("prompt_flow")
+    prompt_flow = cl.user_session.get("config")["active_promptflow"]
     client = pf.PFClient()
  
     response = await cl.make_async(client.test)(prompt_flow, 
@@ -57,26 +65,57 @@ async def call_promptflow(chat_history, message):
                                                           "question": message.content})
     return response
 
+async def activate_promptflow(command: str, command_id: str):
+    config = cl.user_session.get("config")
+    if len(command.split(" ")) < 2:
+        await cl.Message(content=f"#### Promptflow is currently set to `{config['active_promptflow']}`").send()
+        return
+    
+    promptflow_number = int(command.split(" ")[1])
+    if promptflow_number < 0 or promptflow_number >= len(config["promptflows"]):
+        await cl.Message(content=f"#### Invalid promptflow number `{promptflow_number}` -- needs to be between 0 and {len(config['promptflows'])}").send()
+        return
+
+    config["active_promptflow"] = config["promptflows"][promptflow_number]
+    await cl.Message(content=f"#### Set promptflow to `{config['active_promptflow']}`").send()
+
+    cl.user_session.set(
+        "chat_history",
+        [],
+    )
 
 @cl.on_message
 async def run_conversation(message: cl.Message):
-    print("running conversation")
+    question = message.content 
     chat_history = cl.user_session.get("chat_history")
  
-    response = await call_promptflow(chat_history, message)
-
-    if response["image"]:
-        elements = show_images(response["image"])
+    if question.startswith("/activate"):
+        await activate_promptflow(message.content, message.id)
     else:
-        elements = []  
+        response = await call_promptflow(chat_history, message)
 
-    await cl.Message(content=response["answer"], 
-                        author="Answer",
-                        elements=elements).send()
-    
-    chat_history.append({"inputs": {"question": message.content}, 
-                         "outputs": {"answer": response["answer"]}})
+        if response["image"]:
+            elements = show_images(response["image"])
+        else:
+            elements = []  
+
+        await cl.Message(content=response["answer"], 
+                            author="Answer",
+                            elements=elements).send()
+        
+        chat_history.append({"inputs": {"question": message.content}, 
+                            "outputs": {"answer": response["answer"]}})
 
 if __name__ == "__main__":
+
+    # import phoenix as px
+    # from phoenix.trace.exporter import HttpExporter
+    # from phoenix.trace.openai.instrumentor import OpenAIInstrumentor
+    # from phoenix.trace.tracer import Tracer
+
+    # # session = px.launch_app()
+    # tracer = Tracer(exporter=HttpExporter(port=6006))
+    # OpenAIInstrumentor(tracer).instrument()
+
     from chainlit.cli import run_chainlit
     run_chainlit(__file__)
